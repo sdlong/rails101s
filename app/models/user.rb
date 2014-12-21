d35664a032ded822644a57dfc8f3d782c6a89fc9
sdlong <sdlong.jeng@gmail.com>
2014-12-22 06:20:04 +0800

    setting quit and join group in models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@ class User < ActiveRecord::Base
   def is_member_of?(group)
     participated_groups.include?(group)
   end
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
 end
