8117d73d0ef752d36ff139c791401fb878217bc6
sdlong <sdlong.jeng@gmail.com>
2014-12-22 06:21:47 +0800

    setting quit and join group in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :groups do
     resources :posts, except: [:show, :index]
+
+    member do
+      post :join
+      post :quit
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
