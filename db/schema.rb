ece212c1ee4bdc75d2e1b6e77153cb0dce3a9f1f
sdlong <sdlong.jeng@gmail.com>
2014-12-22 03:04:09 +0800

    rake db:migrate add_user_id_to_group

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221184410) do
+ActiveRecord::Schema.define(version: 20141221190320) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
