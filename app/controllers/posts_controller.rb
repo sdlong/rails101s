3a70b6000740e32bdbf49f99d758cfdc36d5199c
sdlong <sdlong.jeng@gmail.com>
2014-12-22 01:55:36 +0800

    posts/update done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,17 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功！"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
