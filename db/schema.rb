47527d84a46c2d39b14a5bc8cef401241db77f6e
sdlong <sdlong.jeng@gmail.com>
2014-12-22 02:44:43 +0800

    rake db:migrate add_name_to_user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221181905) do
+ActiveRecord::Schema.define(version: 20141221184410) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20141221181905) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
