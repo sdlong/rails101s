ad596fefc9976b5574d78cfbbf79c8918ff11544
sdlong <sdlong.jeng@gmail.com>
2014-12-22 01:53:36 +0800

    posts/create done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,21 @@ class PostsController < ApplicationController
     @group = Group.find(params[:group_id])
     @post = @group.posts.new
   end
+  
+  def create
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.build(post_params)
+
+    if @post.save
+      redirect_to group_path(@group), notice: "新增文章成功！"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
