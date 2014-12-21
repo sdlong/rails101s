cc84bd830a4e52eb8746c96119946579958677de
sdlong <sdlong.jeng@gmail.com>
2014-12-22 02:50:14 +0800

    User 可以進入帳號設定，並修改自己的名字

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
   end
 end
