c5adb9b0d86cb8253e12adf300a98c48e900e687
sdlong <sdlong.jeng@gmail.com>
2014-12-22 03:22:16 +0800

    只有 current_user 跟 post.user_id 相同才能在前台顯示 edit / destroy 按鈕

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
 
   belongs_to :group
   belongs_to :author, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(user)
+    user && user == author
+  end
 end
