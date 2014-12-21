a79fce9d298c25646b041c805dc846dbe85ffc84
sdlong <sdlong.jeng@gmail.com>
2014-12-22 01:12:41 +0800

    group/create done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,20 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
