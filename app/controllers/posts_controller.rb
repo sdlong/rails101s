62ba45ec82c77b10a6d1a8fc44de82c18bb5520c
sdlong <sdlong.jeng@gmail.com>
2014-12-22 06:14:24 +0800

    posts/create, edit, update, destroy 加入 User_id 參數

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def create
     @post = @group.posts.build(post_params)
+    @post.author = current_user
 
     if @post.save
       redirect_to group_path(@group), notice: "新增文章成功！"
@@ -22,6 +23,7 @@ class PostsController < ApplicationController
 
   def update
     @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
 
     if @post.update(post_params)
       redirect_to group_path(@group), notice: "文章修改成功！"
@@ -32,6 +34,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post = @group.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
 
     @post.destroy
     redirect_to group_path(@group), alert: "文章已刪除"
