de9bcb26ed89fe44832942888410b3bb1f805dd4
sdlong <sdlong.jeng@gmail.com>
2014-12-26 07:53:50 +0800

    後台的 post 排列要以最新 => 最舊順序排列

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,6 +2,6 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts
+    @posts = current_user.posts.order("updated_at DESC")
   end
 end
