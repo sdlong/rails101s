3b874ad94726b1e7dfcd94bf730ad28edb1879e5
sdlong <sdlong.jeng@gmail.com>
2014-12-26 07:58:18 +0800

    解決 account/posts/index 的 post.group.tiele 的 law of demeter 問題

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   belongs_to :group, counter_cache: :posts_count
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
+  delegate :title, to: :group, prefix: true
+
   def editable_by?(user)
     user && user == author
   end
