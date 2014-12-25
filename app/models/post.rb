1e0ff282be55152ae1b5c14ac5f9542e427c116e
sdlong <sdlong.jeng@gmail.com>
2014-12-26 07:57:23 +0800

    將 group.posts.count 改用 count_cache => .size

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :content, presence: true
 
-  belongs_to :group
+  belongs_to :group, counter_cache: :posts_count
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
   def editable_by?(user)
