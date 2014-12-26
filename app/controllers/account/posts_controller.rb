8a87869162a3e0a2f0620de09fcaaf54894b3830
sdlong <sdlong.jeng@gmail.com>
2014-12-26 08:18:19 +0800

    使用 scope 整理 query

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,6 +2,6 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.order("updated_at DESC")
+    @posts = current_user.posts.recent
   end
 end
