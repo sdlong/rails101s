865dcd622b361ca7b479153d7f03e56723abf809
sdlong <sdlong.jeng@gmail.com>
2015-01-03 08:18:09 +0800

    setting config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,22 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'rails101s' # 你的 application name
+set :repo_url, 'https://github.com/sdlong/rails101s.git' # 你的 git url
+
+# rbenv 的設定
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, "2.1.2"
+set :rbenv_path, "/usr/local/rbenv"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w(rake gem bundle ruby rails)
+set :rbenv_roles, :all # default value
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/home/apps/rails101s" # deploy 的資料夾位置 (prodution)
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -17,16 +25,16 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_files, %w(config/database.yml config/secrets.yml) # git clone 完成後會從 shared 資料夾 copy 過去的檔案
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push("bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system") # git clone 完成後會從 shared 資料夾 copy 過去的檔案
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -40,7 +48,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
-      #   execute :rake, 'cache:clear'
+        execute :rake, 'cache:clear'
       # end
     end
   end
